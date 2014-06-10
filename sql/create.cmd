5f529dc2f6a66a72215318c819c770c8711e1ad8
Alexander Sirotin <Alexander.Sirotin@gmail.com>
2014-06-11 01:33:15 +0400

    Yii + ExtJS 3.4 тест

---
+++ sql/create.cmd
@@ -0,0 +1,3 @@
+mysql --user=root		--password=1q2w3e4r mysql	< create_user.sql
+mysql --user=root		--password=1q2w3e4r mysql	< drop.sql
+mysql --user=eds_sav	--password=1q2w3e4r 		< eds_sav.sql 
---
+++ sql/create_user.sql
@@ -0,0 +1,3 @@
+drop user 'eds_sav'@'%';
+create user 'eds_sav'@'%' identified by '1q2w3e4r';
+grant all on eds_sav.* to 'eds_sav'@'%';
